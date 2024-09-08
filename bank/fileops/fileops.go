8731274cf35c21634e9c4dc482428128d7f9ca5d
harshalsls <harshalsls41069@gmail.com>
2024-09-08 18:35:59 +0530

    adding investment calculator

--- bank/fileops/fileops.go
+++ bank/fileops/fileops.go
@@ -1,9 +1,11 @@
 package fileops
 
 func Read() {
+	//empty read function to check the packaging strategy for Go
 
 }
 
 func Write() {
+	//empty write function to check the packaging strategy for Go
 
 }
